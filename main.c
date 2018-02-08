ad7943685bfcf4fc8c7f2af5adc3ab003c9a05c4
Klaus Kleiner <kkleiner@de.pepperl-fuchs.com>
2018-02-08 15:51:50 +0100

    still fixing bugs

--- main.c
+++ main.c
@@ -11,3 +11,4 @@ Di, 24. Okt 2017 16:35:18
 Di,  6. Feb 2018 08:06:18
 stabilization for release candidate 1.2.3
 stabilization for release candidate 1.2.4
+stabilization for release candidate 1.2.4 still fixing bugs
